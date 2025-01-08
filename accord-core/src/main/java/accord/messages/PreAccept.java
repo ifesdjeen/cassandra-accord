248eb15fb531a0372e236545a88bf1fd8c8dff1e
Alex Petrov <oleksandr.petrov@gmail.com>
2025-01-08 18:11:20 +0100

    Revert change related to preload context

--- accord-core/src/main/java/accord/messages/PreAccept.java
+++ accord-core/src/main/java/accord/messages/PreAccept.java
@@ -255,11 +255,10 @@ public class PreAccept extends WithUnsynced<PreAccept.PreAcceptReply>
     {
         // TODO (expected): do not build covering ranges; no longer especially valuable given use of FullRoute
         // NOTE: ExclusiveSyncPoint *relies* on STARTED_BEFORE to ensure it reports a dependency on *every* earlier TxnId that may execute (before or after it).
-        Participants<?> intersecting = participants.touches().intersecting(safeStore.context().keys());
         try (Deps.AbstractBuilder<Deps> builder = new Deps.Builder(true);
              RangeDeps.BuilderByRange redundantBuilder = RangeDeps.builderByRange())
         {
-            RangeDeps redundant = safeStore.redundantBefore().collectDeps(intersecting, redundantBuilder, minEpoch, executeAt).build();
+            RangeDeps redundant = safeStore.redundantBefore().collectDeps(participants.touches(), redundantBuilder, minEpoch, executeAt).build();
             if (nullIfRedundant && !txnId.is(EphemeralRead))
             {
                 TxnId maxRedundantBefore = redundant.maxTxnId(null);
@@ -270,7 +269,7 @@ public class PreAccept extends WithUnsynced<PreAccept.PreAcceptReply>
                 }
             }
 
-            safeStore.visit(intersecting, executeAt, txnId.witnesses(),
+            safeStore.visit(participants.touches(), executeAt, txnId.witnesses(),
                             (p1, in, keyOrRange, testTxnId) -> {
                                 if (p1 == null || !testTxnId.equals(p1))
                                     in.add(keyOrRange, testTxnId);
